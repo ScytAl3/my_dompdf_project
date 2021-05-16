2d90f9f04f0b26090d75a9bb31c5b963da4ca393
ScytAl3 <45272552+ScytAl3@users.noreply.github.com>
2021-05-16 15:52:37 +0200

    Update assets files and yarn upgrade dependencies

---
+++ assets/pdf.js
@@ -0,0 +1,1 @@
+import './styles/pdf.css';
--- webpack.config.js
+++ webpack.config.js
@@ -35,8 +35,8 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/js/app.js')
-    .addEntry('pdf', './assets/js/pdf.js')
+    .addEntry('app', './assets/app.js')
+    .addEntry('pdf', './assets/pdf.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
